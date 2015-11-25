c61f3806d4fe5ec419b95314004e36897ad8709e
Kimar Arakaki Neves <kimar@kimar.eng.br>
2015-11-24 22:07:20 -0800

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kimar Arakaki Neves on 2015-11-24.
+//  Copyright © 2015 Kimar Arakaki Neves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
